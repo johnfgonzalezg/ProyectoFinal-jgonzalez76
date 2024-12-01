d12eceac9c119001580078d39fe65c4b5fcc4e8c
John Fredy González Gutiérrez <johnfgonzalezg@gmail.com>
2024-11-30 23:07:53 -0500

    Corrección error creación de tablas en BD

--- db.py
+++ db.py
@@ -1,9 +1,8 @@
 from flask_sqlalchemy import SQLAlchemy
-
-
+from sqlalchemy_utils import database_exists
 db = SQLAlchemy()
 
 def init_db(app):
     with app.app_context():
-        db.drop_all()
-        db.create_all()
+        if not database_exists(app.config["SQLALCHEMY_DATABASE_URI"]):
+            db.create_all()
