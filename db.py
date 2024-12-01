de4d625b45866018f423ebe25a7b723099c769a0
John Fredy González Gutiérrez <johnfgonzalezg@gmail.com>
2024-11-30 23:21:53 -0500

    reajuste a bd.py para crear tablas y data de bd

--- db.py
+++ db.py
@@ -1,8 +1,9 @@
 from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy_utils import database_exists
+
+
 db = SQLAlchemy()
 
 def init_db(app):
     with app.app_context():
-        if not database_exists(app.config["SQLALCHEMY_DATABASE_URI"]):
-            db.create_all()
+        db.drop_all()
+        db.create_all()
